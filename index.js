d490367c9633357f9f9cefa4fc85aab387fa9147
sagar <sagarsrivastava347@gmail.com>
2026-03-20 01:30:24 +0530

    all new files

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const app = express();
 let port = 8080;
 const path = require("path")
 const Chat= require ("./models/chat.js")
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 app.listen(port,(req, res)=>{
@@ -22,8 +24,7 @@ app.get("/", (req,res)=>{
 
 //index route
 app.get ("/Chats", async (req,res)=>{
-  let Chats = await Chat.find();
-  console.log (Chats)
+  let Chats = await Chat.find(); 
   res.render("index.ejs",{Chats} )
     
 })
@@ -32,18 +33,51 @@ app.get("/Chat/new", (req,res)=>{
     res.render("new.ejs")
 })
 
+app.get("/Chats/:id/edit", async (req,res)=>{
+  let {id} = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", {chat})
+})
+//update route 
+
+app.put("/Chats/:id", async (req,res)=>{
+  let {id} = req.params;
+  let {newMsg}= req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators: true ,new : true});
+  console.log (updatedChat);
+  res.redirect("/Chats")
+  console.log(req.body);
+  
+});
+//DISTROY ROUTE
+
+app.delete("/Chats/:id", async (req,res)=>{
+  let {id}= req.params;
+  chatToBeDeleted = await Chat.findByIdAndDelete(id)
+  console.log (chatToBeDeleted);
+  res.redirect("/Chats")
+})
+
+
 app.post("/Chats", (req,res)=>{
     let {from , to , msg}= req.body;
     let newChat = new Chat({
       from : from ,
       to : to ,
       msg : msg,
-      created_at : new Data()
-    });
-     console.log (newChat);
-     res.send("working")
+      created_at : new Date()
+    })
+     newChat.save().then((res)=>{
+      console.log("chat was saved")
+     }).catch((err)=>{
+         console.log(err)
+     })
 
+ res.redirect("/Chats")
 })
+
+
+
 //mongoose setting
  const mongoose = require ("mongoose");
 
